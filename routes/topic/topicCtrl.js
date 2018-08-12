19bc8f98c3684b1444e10a8b16bb80517cc2f269
Jepil Choi <myartame@gmail.com>
2018-08-12 17:58:51 +0900

    [ADD] getTopics controller logic

--- routes/topic/topicCtrl.js
+++ routes/topic/topicCtrl.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const createError = require('http-errors');
+const topicModel = require('../../models/topicModel');
+
 module.exports = {
-    getTopics : (req, res) => {
-        res.sendStatus(200);
+    getTopics : async (req, res, next) => {
+        try {
+            const topics = await topicModel.getTopics();
+
+            res
+                .status(200)
+                .json({ topics });
+        } catch(error) {
+            next(createError(error));
+        }
     },
 
     createTopic : (req, res) => {
